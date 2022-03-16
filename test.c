4ab18e54cbd64f30f42cb83ecb10be3879cd856d
UH5555 <zhuhao162@163.com>
2022-03-16 20:27:31 +0800

    0316\VS环境-c语言调试0-107min

--- test.c
+++ test.c
@@ -27,19 +27,75 @@
 //	return 0;
 //}
 
-//F5----启动调试，经常用来调到下一个断点
-//F9----设置断点
-int main()
-{
-	int i = 0;
-	for (i = 0; i < 100; i++)
-	{
-		printf("%d ", i);
-	}
-	for (i = 0; i < 100; i++)
-	{
-		printf("%d ", 10-i);
-	}
-	//system("pause");
-	return 0; 
-}
+////F5----启动调试，经常用来调到下一个断点
+////F9----设置断点
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", 10-i);
+//	}
+//	//system("pause");
+//	return 0; 
+//}
+
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = Add(a, b);
+//	return 0;
+//}
+ 
+//void test2()
+//{
+//	printf("hehe\n");
+//}
+//
+//void test1()
+//{
+//	test2();
+//}
+//
+//void test()
+//{
+//	test1();
+//}
+//
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+
+//1!+2!+3!+...+n!
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;//保存最终结果
+//	int n = 0;
+//	int ret = 1;//保存n的阶乘
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			ret *= j;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
