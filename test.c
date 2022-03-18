90ce07f4e37d5a8555b7451b1173df6e17d6209a
UH5555 <zhuhao162@163.com>
2022-03-18 18:40:46 +0800

    0318\VS环境-c语言调试（ALL）

--- test.c
+++ test.c
@@ -100,58 +100,105 @@
 //	return 0;
 //}
 
-//strcpy - 字符串拷贝
-/////6/10分
-//void my_strcpy(char* dest, char* src)
+////strcpy - 字符串拷贝
+///////6/10分
+////void my_strcpy(char* dest, char* src)
+////{
+////	while (*src != '\0')
+////	{
+////		*dest = *src;
+////		src++;
+////		dest++;
+////	}
+////	*dest = *src;
+////}
+//
+//////7/10分
+////void my_strcpy(char* dest, char* src)
+////{
+////	while (*dest++ = *src++)
+////	{
+////		;
+////	}
+////}
+//
+//////7/10分
+////void my_strcpy(char* dest, char* src)
+////{
+////	if (dest != NULL && src != NULL)
+////	{
+////		while (*dest++ = *src++)
+////		{
+////			;
+////		}
+////	}
+////}
+//
+//
+//////8/10分
+////#include <assert.h>
+////void my_strcpy(char* dest, char* src)
+////{
+////	assert(dest != NULL);//断言
+////	assert(src != NULL);//断言
+////	while (*dest++ = *src++)
+////	{
+////		;
+////	}
+////}
+//
+////10/10分
+//char my_strcpy(char* dest,const char* src)
 //{
-//	while (*src != '\0')
-//	{
-//		*dest = *src;
-//		src++;
-//		dest++;
-//	}
-//	*dest = *src;
-//}
-
-////7/10分
-//void my_strcpy(char* dest, char* src)
-//{
-//	while (*dest++ = *src++)
-//	{
-//		;
-//	}
-//}
-
-////7/10分
-//void my_strcpy(char* dest, char* src)
-//{
-//	if (dest != NULL && src != NULL)
-//	{
-//		while (*dest++ = *src++)
-//		{
-//			;
-//		}
-//	}
-//}
-
-
-////8/10分
-//#include <assert.h>
-//void my_strcpy(char* dest, char* src)
-//{
+//  char *ret = dest;
 //	assert(dest != NULL);//断言
 //	assert(src != NULL);//断言
 //	while (*dest++ = *src++)
 //	{
 //		;
 //	}
+//  return ret;
+//}
+//
+//int main()
+//{
+//	char arr1[] = "#############";
+//	char arr2[] = "bit";
+//	//my_strcpy(arr1, NULL);
+//	printf("%s\n", my_strcpy(arr1, arr2));
+//	return 0;
 //}
 
-int main()
-{
-	char arr1[] = "#############";
-	char arr2[] = "bit";
-	my_strcpy(arr1, NULL);
-	printf("%s\n", arr1);
-	return 0;
-}
+//int main()
+//{
+//	const int num = 10;
+//	const int* p = &num;
+//	err   const 放在指针变量的*的左边时，修饰的是*p。不能通过p来改变*p（num）的值。
+//	int* const p = &num;
+//	const 放在指针变量的*的右边时，修饰的是指针变量p本身，p不能被改变。
+//	*p = 20;
+//
+//	printf("%d\n", num);
+//	return 0;
+//}
+
+//#include <assert.h>
+//int my_strlen(const char* str)
+//{
+//	int count = 0;
+//	assert(str != NULL);//断言
+//
+//	while (*str)
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	printf("%d\n", my_strlen(arr));
+//	return 0;
+//}
